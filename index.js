404fd0de263d1846e4eacccff63472050a71a458
Andre Ranussi <ranussi.a@gmail.com>
2023-06-27 15:37:26 +0100

    started to work with the JavaScript aspect of the calculator

---
+++ index.js
@@ -0,0 +1,12 @@
+var gender = document.querySelector("#gender");
+var genderValue = gender.value;
+
+var ageValue = document.querySelector("#age").value;
+
+var heightValue = document.querySelector("#height").value;
+
+var weightValue = document.querySelector("#weight").value;
+
+var activeLevel = document.querySelector("#activeLevel");
+var activeLevelValue = activeLevel.value;
+
