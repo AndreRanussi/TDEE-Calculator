aaea4e3c2d229d967565f456b5ad69f3554956c8
Andre Ranussi <ranussi.a@gmail.com>
2023-06-28 21:20:40 +0100

    Incorporated the functionality of calcualting using JavaScript

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # TDEE Calculator
 
-Learning the basics of javascript and playing around with Bootstrap.
+On this project, the aim was to get a feel of the Bootstrap framework and to complete my first challenge with Javascript. 
+Despite now feeling simple, it was challenging to figure out how to make Javascript carry out the calculation the way I needed to and then push the result on the DOM at the click of a button. 
 
 <!-- # Hosted
 
@@ -12,9 +13,25 @@ https://andreranussi.github.io/TDEE-Calculator/ -->
 - Bootstrap
 
 # Optional To Do
-Stil being developed!!!!
-- Function to calculate an individual's Total Daily Energy Expenditure
+- Improve on the design using Bootstrap.
+- Improve the accuracy of the calculation by adding the option for the user to porvide their bodyfat percentage.
 
 
 # Usage
+Katch-McArdle formula was used to calculate the Basic Metabolic Rate (BMR), and then the BMR value was multiplied by an activity multiplier factor.
+
+Formula: 
+- Lean Body Mass (LBM) Male = 0.407 × weight [kg] + 0.267 × height [cm]  19.2
+- Lean Body Mass (LBM) Female = 0.252 × weight [kg] + 0.473 × height [cm] - 48.3
+- BMR = 370 + (21.6 × Lean Body Mass [kg])
+
+Activity Level Multiplier:
+- Little/no exercise (sedentary lifestyle) (1.2)
+- Light exercise 1-2 times/week (1.375)
+- Moderate exercise 2-3 times/week (1.55)
+- Hard exercise 4-5 times/week (1.725)
+- Physical job or hard exercise 6-7 times/week (1.9)
+- Professional athlete (2.9)
+
+TDEE = BMR × Activity Level Multiplier
 
--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-function calculate() {
-  var gender = document.querySelector("#gender");
-  var genderValue = gender.value;
-  var heightValue = document.querySelector("#height").value;
-  var weightValue = document.querySelector("#weight").value;
-  var activeLevel = document.querySelector("#activeLevel");
-  var activeLevelValue = activeLevel.value;
-  
-  var maleResult = (370+(21.6*(((0.407*weightValue)+(0.267*heightValue))-19.2)))*activeLevelValue;
-  var femaleResult = (370+(21.6*(((0.252*weightValue)+(0.473*heightValue))-48.3)))*activeLevelValue;
-  
-  if (genderValue === "Male") {
-    return Math.round(maleResult);
-  } else {
-    return Math.round(femaleResult);
-  }
-    
-  }
 
-var submit = document.querySelector("#result").innerText = calculate();
-var button = document.querySelector("#btn");
+  function calculation(){
+    var gender = document.querySelector("#gender");
+    var genderValue = gender.value;
+    var heightValue = document.querySelector("#height").value;
+    var weightValue = document.querySelector("#weight").value;
+    var activeLevel = document.querySelector("#activeLevel");
+    var activeLevelValue = activeLevel.value;
   
-button.onclick = function(){submit};
+    var maleResult = Math.round((370 + (21.6 * (((0.407 * weightValue) + (0.267 * heightValue)) - 19.2))) * activeLevelValue);
+    var femaleResult = Math.round((370 + (21.6 * (((0.252 * weightValue) + (0.473 * heightValue)) - 48.3))) * activeLevelValue);
+    if (maleResult <= 0 || femaleResult <= 0 ) {
+    return "0 "
+  } else if (genderValue === "Male") {
+    return maleResult + " ";
+  } else {
+    return femaleResult + " " ;
+  }
+}
+
+function result(){
+  document.querySelector("#result").innerHTML = calculation();
+}
