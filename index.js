62cef522d9e5b0759268ae4fb9bc024fcf570d61
Andre Ranussi <ranussi.a@gmail.com>
2023-06-28 07:34:36 +0100

    figured out the calculation in javascript

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-var gender = document.querySelector("#gender");
-var genderValue = gender.value;
+function calculate() {
+  var gender = document.querySelector("#gender");
+  var genderValue = gender.value;
+  var heightValue = document.querySelector("#height").value;
+  var weightValue = document.querySelector("#weight").value;
+  var activeLevel = document.querySelector("#activeLevel");
+  var activeLevelValue = activeLevel.value;
+  
+  var maleResult = (370+(21.6*(((0.407*weightValue)+(0.267*heightValue))-19.2)))*activeLevelValue;
+  
+  var femaleResult = (370+(21.6*(((0.252*weightValue)+(0.473*heightValue))-48.3)))*activeLevelValue;
+  
+  if (genderValue === "Male") {
+    return Math.round(maleResult);
+  } else {
+    return Math.round(femaleResult);
+  }
+    
+  }
 
-var ageValue = document.querySelector("#age").value;
-
-var heightValue = document.querySelector("#height").value;
-
-var weightValue = document.querySelector("#weight").value;
-
-var activeLevel = document.querySelector("#activeLevel");
-var activeLevelValue = activeLevel.value;
-
+  
+  
